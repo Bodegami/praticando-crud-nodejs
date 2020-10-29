3b2182534e4508a58456590077261017fb4b713c
Renato <rena.f.oliveira@gmail.com>
2020-10-29 01:06:52 -0300

    Add CRUD/ Pagination/ Cors

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
-// Iniciando o App
-const app = express();
-
-// Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi',  {
+const mongooseOptions = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    });
+    useFindAndModify: false,
+    
+    };
+
+// Iniciando o App
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', mongooseOptions);
 
 // Sem o require dir = require('./src/models/Product');
 requireDir('./src/models');
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,11 +1,50 @@
 const mongoose = require('mongoose');
 
+
 const Product = mongoose.model('Product');
 
 module.exports = {
+
+    //GET ALL PRODUCTS
     async index(request, response) {
-        const products = await Product.find();
+        //const products = await Product.find();
+        const { page = 1 } = request.query;
+        const products = await Product.paginate({}, {page, limit: 10});
 
         return response.json(products);
     },
-}
+
+
+    //GET ONE PRODUCT BY ID
+    async show(request, response) {
+        const product = await Product.findById(request.params.id);
+
+        return response.json(product);
+    },
+
+
+    //CREATE NEW PRODUCT
+    async store(request, response) {
+        const product = await Product.create(request.body);
+
+        return response.json(product);
+    },
+
+
+    //UPDATE PRODUCT
+    async update(request, response) {
+        const product = await Product.findByIdAndUpdate(request.params.id, request.body, { new: true });
+
+        return response.json(product);
+    },
+
+
+    //DELETE PRODUCT
+    async destroy(request, response) {
+        await Product.findByIdAndRemove(request.params.id);
+
+        return response.send();
+    }
+
+    
+};
